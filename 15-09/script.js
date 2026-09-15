d90422eaf82fe5b1cb58eef9572866720ff1e071
Odirlei <odirlei@email.com>
2026-09-15 11:38:43 -0300

    adicionada pasta 15-09 - variaveis

---
+++ 15-09/script.js
@@ -0,0 +1,33 @@
+// let miranha = "Peter Parker";
+// let numero = 999999999;
+// let numero2 = "999999999";
+// let idade = 17;
+// var nome = "odirlei";
+// nome = "Pedro";
+
+// const nome = "odirlei";
+// nome = "Pedro";
+
+// idade = 20;
+// idade = 21;
+
+// console.log("O miranha é o " + nome + " e ele tem " + idade + " anos");
+
+// Crie 4 variaveis contendo: nome, sobrenome, idade e curso
+// Exiba no console em uma unica frase
+
+// let nome = "odirlei"; //string
+// let peso = 90; //int
+// let altura = 1.80; //float
+// let funcionario = false;//bool - true == verdadeiro ou false == falso
+
+// let cidade = null;
+// let estado;
+// console.log(typeof nome);
+// console.log(nome);
+// console.log(typeof peso);
+// console.log(typeof altura);
+// console.log(typeof funcionario);
+
+// console.log("A variavel nome tem " + nome + "e é do tipo " + typeof nome);
+
