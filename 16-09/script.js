789e26bcf311a6df844c0f4d2d701065ce8e8220
Odirlei <odirlei@email.com>
2026-09-16 11:56:33 -0300

    criação da pasta 16-09 e estrutura condicional

---
+++ 16-09/script.js
@@ -0,0 +1,99 @@
+// operadores aritimeticos
+// + soma
+// - subtração
+// * multiplicação
+// / divisão
+// % sobra da divisão
+
+// let n1 = 3;
+// let n2 = 9;
+
+// console.log(n1 + n2);
+// console.log(n2 - n1);
+// console.log(n1 * n2);
+// console.log(n2 / n1);
+// console.log(n2 % n1);
+// console.log((n2 - n1) * 2);
+// console.log("palavra" + n1);
+
+//Crie 3 variaveis, cada uma contendo um numero,
+// some as 3 e tire a média, exiba o resultado no console
+
+//Agora exiba no console a formula do calculo
+//com os valores e seu resultado
+//A média de X, X e X é R
+
+// > maior que
+// < menor que
+// == igual
+// >= maior ou igual
+// <= menor ou igual
+// != é diferente
+
+//let idade = 18;
+
+// console.log(idade > 18);
+// console.log(idade < 18);
+// console.log(idade == 18);
+// console.log(idade >= 18);
+// console.log(idade <= 18);
+// console.log(idade != 18);
+//console.log(idade === "18");//comparação de tipo de variavel
+
+// let idade = 18;
+// let habilitado = true;
+// console.log(idade <= 18 && habilitado == true);
+// V && V = V
+// V && F = F
+// F && V = F
+// F && F = F
+
+// console.log(idade <= 18 || habilitado == true);
+// V || V = V
+// V || F = V
+// F || V = V
+// F || F = F
+
+// ! operador NOT "inverte"
+// console.log(!habilitado);
+
+//se idade for igual a 18
+    //faça isso
+
+//se não
+    //faça isso
+// let habilitado = false;
+
+// if (habilitado == true) {// se sim
+//     console.log("Pode dirigir");
+    
+// } else {//se não
+//     console.log("NÃO pode dirigir");
+// }
+
+let idade = 16;
+
+// if (idade >= 18)
+if (idade > 17) {
+    console.log("Maior de idade!");
+    
+} else {
+    console.log("Menor de idade");
+}
+//Ex 1
+//Crie um sistema que compare o valor de uma variavel
+//senha SE for igual a 1234 escreva no console 
+// "acesso liberado" SE NÃO escreva "acesso bloqueado"
+
+//Ex 2
+//Crie um sistema que avalie uma variavel nota
+// SE nota for maior que 6 escreva aprovado, SE NAO
+// escreva reprovado
+
+//Ex 3
+//Crie um sistema que compara um valor total de uma
+//compra, SE o valor for maior que 500 calcule um 
+//desconto de 20 e mostre o valor total da compra 
+//com desconto SE NAO mostre a mensagem 
+// COMPRA SEM DESCONTO
+
